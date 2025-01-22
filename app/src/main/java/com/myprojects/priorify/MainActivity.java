5675f7e0816debf47de313984dc7ff17d7c16c64
Tinty <vladimirhomutov55917@gmail.com>
2025-01-23 00:07:39 +0300

    Initial commit

---
+++ app/src/main/java/com/myprojects/priorify/MainActivity.java
@@ -0,0 +1,25 @@
+package com.myprojects.priorify;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View view) {
+        Toast.makeText(this, "!!!", Toast.LENGTH_SHORT).show();
+    }
+}

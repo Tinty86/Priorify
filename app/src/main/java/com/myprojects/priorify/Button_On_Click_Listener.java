99271584f5ea1ebd91a1b53fc60ca42d3ae933fd
Tinty <vladimirhomutov55917@gmail.com>
2025-02-09 17:28:58 +0300

    refactor(java, xml): refactor onClick method
    Refactored onClick in activity_main.xml to onClick listener.

---
+++ app/src/main/java/com/myprojects/priorify/Button_On_Click_Listener.java
@@ -0,0 +1,27 @@
+package com.myprojects.priorify;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+public class Button_On_Click_Listener implements View.OnClickListener{
+
+    // getApplicationContext()
+    Context context;
+
+    public Button_On_Click_Listener(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+        AppCompatButton BT = (AppCompatButton) view;
+        Intent intent = new Intent(context, HistoryActivity.class);
+        intent.putExtra("block_name", BT.getText().toString());
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}
--- app/src/main/java/com/myprojects/priorify/MainActivity.java
+++ app/src/main/java/com/myprojects/priorify/MainActivity.java
@@ -14,17 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppCompatButton BT_im_ur;
+    AppCompatButton BT_non_im_ur;
+    AppCompatButton BT_im_non_ur;
+    AppCompatButton BT_non_im_non_ur;
+    Button_On_Click_Listener MyListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyListener = new Button_On_Click_Listener(getApplicationContext());
+
+        BT_im_ur = findViewById(R.id.high_left_button);
+        BT_non_im_ur = findViewById(R.id.low_left_button);
+        BT_im_non_ur = findViewById(R.id.high_right_button);
+        BT_non_im_non_ur = findViewById(R.id.low_right_button);
+
+        BT_im_ur.setOnClickListener(MyListener);
+        BT_non_im_ur.setOnClickListener(MyListener);
+        BT_im_non_ur.setOnClickListener(MyListener);
+        BT_non_im_non_ur.setOnClickListener(MyListener);
     }
 
-    public void onClick(View view) {
-        AppCompatButton BT = findViewById(view.getId());
-        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
-        Toast.makeText(this, BT.getText().toString(), Toast.LENGTH_SHORT).show();
-        intent.putExtra("block_name", BT.getText().toString());
-        startActivity(intent);
-    }
+//    public void onClick(View view) {
+//        AppCompatButton BT = findViewById(view.getId());
+//        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+//        Toast.makeText(this, BT.getText().toString(), Toast.LENGTH_SHORT).show();
+//        intent.putExtra("block_name", BT.getText().toString());
+//        startActivity(intent);
+//    }
 }
